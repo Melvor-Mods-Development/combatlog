b0eb6c89c561ffb8ba09a5575818da6426e53342
mazunki <rolferen@gmail.com>
2022-10-27 11:47:40 +0200

    functions make it easier to read

--- add_data_filters.js
+++ add_data_filters.js
@@ -54,25 +54,16 @@ function get_all_combat_drops() {
 	return found_items;
 }
 
-function get_all_available_items() {
-	/*
-	 * returns a list of all items a player will be able to get on a (combat-only) character
-	 *
-	 * this should actually take a list of all items we're starting with, and recursively
-	 * open, craft, upgrade, buy, e.g... instead of using hardcoded found_items
-	 *
-	 * maybe we should create functions for individual sources. would be cleaner
-	 */
-	let found_items = get_all_combat_drops();
-	let new_stuff = true;
+function get_new_buyable_items(current_items) {
+	let freshItems = [];
 
 	if (SHOP.Materials.length === undefined) return;
 	for (let i=0; i<SHOP.Materials.length; i++) {
 		if(SHOP.Materials[i].name === "Weird Gloop") continue; // Ignore weird gloop as we cannot get rune essence to buy it.
 		let grocery = SHOP.Materials[i];
 		for (let j=0; j<grocery.contains.items.length; j++) {
-			if (found_items.indexOf(grocery.contains.items[j][0]) == -1) { // avoid dupes
-				found_items[found_items.length] = grocery.contains.items[j][0];
+			if (current_items.indexOf(grocery.contains.items[j][0]) == -1) { // avoid dupes
+				freshItems[freshItems.length] = grocery.contains.items[j][0];
 			}
 		}
 
@@ -87,22 +78,22 @@ function get_all_available_items() {
 
 		let grocery = SHOP.Slayer[i];
 		for (let j=0; j<grocery.contains.items.length; j++) {
-			if (found_items.indexOf(grocery.contains.items[j][0]) == -1) { // avoid dupes
-				found_items[found_items.length] = grocery.contains.items[j][0];
+			if (current_items.indexOf(grocery.contains.items[j][0]) == -1) { // avoid dupes
+				freshItems[freshItems.length] = grocery.contains.items[j][0];
 			}
 		}
 	}
-
 
 	if (SHOP.Gloves.length === undefined) return;
 	for (let i=0; i<SHOP.Gloves.length; i++) {
 		let grocery = SHOP.Gloves[i];
 		for (let j=0; j<grocery.contains.items.length; j++) {
-			if (found_items.indexOf(grocery.contains.items[j][0]) == -1) { // avoid dupes
-				found_items[found_items.length] = grocery.contains.items[j][0];
+			if (current_items.indexOf(grocery.contains.items[j][0]) == -1) { // avoid dupes
+				freshItems[freshItems.length] = grocery.contains.items[j][0];
 			}
 		}
 	}
+
 	if (SHOP.Skillcapes.length === undefined) return;
 	for (let i=0; i<SHOP.Skillcapes.length; i++) {
 		let cape = SHOP.Skillcapes[i];
@@ -122,45 +113,81 @@ function get_all_available_items() {
 		}
 
 		for (let j=0; j<cape.contains.items.length; j++) {
-			if (found_items.indexOf(cape.contains.items[j][0]) == -1) { // avoid dupes
-				found_items[found_items.length] = cape.contains.items[j][0];
+			if (current_items.indexOf(cape.contains.items[j][0]) == -1) { // avoid dupes
+				freshItems[freshItems.length] = cape.contains.items[j][0];
+			}
+		}
+	}
+	return freshItems;
+}
+
+function get_new_chest_items(current_items) {
+	let freshItems = [];
+
+	for (let i=0; i<current_items.length; i++) {
+		if (items[current_items[i]].canOpen) {
+			let dt = items[current_items[i]].dropTable;
+			for (let j=0; j<dt.length; j++) {
+				if (current_items.indexOf(dt[j][0]) == -1) { // avoid dupes
+					freshItems[freshItems.length] = dt[j][0];
+				}
 			}
 		}
 	}
 
+	return freshItems;
+}
 
-	while (new_stuff) { // this will recursively open and craft chests
-		new_stuff = false;
-		// buying stuff from shop
-		// opening all chests
-		for (let i=0; i<found_items.length; i++) {
-			if (items[found_items[i]].canOpen) {
-				let dt = items[found_items[i]].dropTable;
-				for (let j=0; j<dt.length; j++) {
-					if (found_items.indexOf(dt[j][0]) == -1) { // avoid dupes
-						found_items[found_items.length] = dt[j][0];
-						new_stuff = true;
-					}
+function get_new_craftable_items(current_items) {
+	let freshItems = [];
+
+	for (let i=0; i<items.length; i++) {
+		if (items[i].itemsRequired !== undefined) {
+			let qualified = true;
+			let ingredients = items[i].itemsRequired;
+			for (let j=0; (j<ingredients.length) && (qualified); j++) {
+				if (current_items.indexOf(ingredients[j][0]) == -1)
+					qualified = false;  // missing an ingredient
+			}
+			if (qualified) {
+				if (found_items.indexOf(i) == -1) { // avoid dupes
+					freshItems[freshItems.length] = i;
 				}
 			}
 		}
+	}
 
-		// going through all craftable items
-		for (let i=0; i<items.length; i++) {
-			if (items[i].itemsRequired !== undefined) {
-				let qualified = true;
-				let ingredients = items[i].itemsRequired;
-				for (let j=0; (j<ingredients.length) && (qualified); j++) {
-					if (found_items.indexOf(ingredients[j][0]) == -1)
-						qualified = false;  // missing an ingredient
-				}
-				if (qualified) {
-					if (found_items.indexOf(i) == -1) { // avoid dupes
-						found_items[found_items.length] = i;
-						new_stuff = true;
-					}
-				}
-			}
+	return freshItems;
+}
+
+function get_all_available_items(current_items) {
+	/*
+	 * returns a list of all items a player will be able to get on a (combat-only) character
+	 *
+	 * this should actually take a list of all items we're starting with, and recursively
+	 * open, craft, upgrade, buy, e.g... instead of using hardcoded found_items
+	 *
+	 * maybe we should create functions for individual sources. would be cleaner
+	 */
+	let new_stuff = true;
+
+	while (new_stuff) { // this will recursively open and craft chests
+		new_stuff = false;
+		let new_shop_items = get_new_buyable_items(current_items);
+		let new_chest_items = get_new_chest_items(current_items);
+		let new_craftable_items = get_new_craftable_items(current_items);
+
+		for (let i=0; i<new_shop_items.length(); i++) {
+			current_items[freshItems.length] = new_shop_items[i];
+			new_stuff = true;
+		}
+		for (let i=0; i<new_chest_items.length(); i++) {
+			current_items[freshItems.length] = new_chest_items[i];
+			new_stuff = true;
+		}
+		for (let i=0; i<new_craftable_items.length(); i++) {
+			current_items[freshItems.length] = new_craftable_items[i];
+			new_stuff = true;
 		}
 	}
 
